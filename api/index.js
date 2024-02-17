45d286f6c3b8d3fdc2d5496cc822b84f192d2a63
Aditya Bhandari <adityabhandari0050@gmail.com>
2024-02-18 01:50:07 +0545

    update for vercel

--- api/index.js
+++ api/index.js
@@ -89,6 +89,4 @@ app.post("/api/upload", upload.single("uploadedImage"), async (req, res) => {
   }
 });
 
-app.listen(4000, () => {
-  console.log("Server is up and running on port 4000");
-});
+module.exports = app;
